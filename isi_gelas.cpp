6dfc4a20bb76f00a57e772f43ff180a4232c4f6b
kyo21 <kussa.laksana@gmail.com>
2024-02-06 12:38:37 +0800

    adding file pdf and cpp

---
+++ isi_gelas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+//<---- Dilarang menambahkan kode di atas batas ini----->
+
+int waktu_isi_gelas(){
+
+
+}
+
+int main(){
+
+return 0;
+}
